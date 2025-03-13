8dbec428cd1f7b2788767073f586aac8e0b615a2
irisbaltus <150801536+irisbaltus@users.noreply.github.com>
2025-03-13 21:50:52 +0100

    routes aangemaakt

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app
     .set('view engine', 'ejs') // stelt EJS in als template engine
@@ -7,9 +7,30 @@ app
 
 app
     .get('/', onhome)
+    .get('/quiz', onquiz)
+    .get('/favorieten', onfavorieten)
+    .get('/resultaten', onresultaten)
+    .get('/detail', ondetail)
     .listen(8000)
 
 function onhome(req, res){
-    res.send('<h1></h1>')
+    res.send('<h1>Welkom bij de plantenmatch</h1>')
+
+}
+function onquiz(req, res){
+    res.send('<h1>Welkom bij de quiz</h1>')
+    
+}
+function onfavorieten(req, res){
+    res.send('<h1>Welkom bij jouw favorieten</h1>')
+    
+}
+function onresultaten(req, res){
+    res.send('<h1>Welkom bij jouw resultaten</h1>')
+    
+}
+function onhome(req, res){
+    res.send('<h1>Welkom bij de detailpagina</h1>')
+    
 }
 
