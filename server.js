d7ea3e285d978a42b8bec6439671e166e8b52f39
irisbaltus <150801536+irisbaltus@users.noreply.github.com>
2025-03-17 13:56:00 +0100

    .listen onderin

--- server.js
+++ server.js
@@ -12,13 +12,15 @@ app.set('views', 'view')
 
 
 app.get('/', onhome)
-    .listen(9000, () => {
-    console.log('Server is running on http://localhost:9000');
-  
+   
     .get('/quiz', onquiz)
     .get('/favorieten', onfavorieten)
     .get('/resultaten', onresultaten)
     .get('/detail', ondetail)
+
+    .listen(9000, () => {
+        console.log('Server is running on http://localhost:9000');
+      
 })
 
 function onhome(req, res) {
